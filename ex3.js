445314eff934402fd971aef4bf4fc200fd4cb3e0
unknown <ryanchehab20@gmail.com>
2024-10-27 20:18:00 +0200

    generated the tree and added styling

--- ex3.js
+++ ex3.js
@@ -1,13 +1,23 @@
 let height = prompt("Enter the height of the tree: ") 
+height = parseInt(height);
 
 // main condition
 if (!isNaN(height) && height!== "" && height>0){
+
     console.log(height)
-
-    for(let i = 0; i<height;i++){
-
+    let container = document.createElement("div")
+    container.classList.add('container')
+    
+    // generating the tree
+    for (let x = 0; x < height; x++) {
+        let volume = " ".repeat(height - x - 1)+ "*" + "  |  ".repeat(x) + "*";
+        let tree = document.createElement("div");
+        tree.innerHTML = volume;
+        container.appendChild(tree);
     }
+    
+    document.body.appendChild(container)
 }
 else{
-    console.log("invalid input")
+    alert("Please enter a valid height")
 }

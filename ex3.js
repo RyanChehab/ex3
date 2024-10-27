13064b8f3f940fedb99fcd5814f72d89c817bc8d
unknown <ryanchehab20@gmail.com>
2024-10-27 19:37:20 +0200

    built the file structure

---
+++ ex3.js
@@ -0,0 +1,13 @@
+let input = prompt("Enter a positive number: ") 
+
+// main condition
+if (!isNaN(input) && input!== "" && input>0){
+    console.log(input)
+
+    for(let i = 0; i<input;i++){
+        
+    }
+}
+else{
+    console.log("incorrect nb")
+}

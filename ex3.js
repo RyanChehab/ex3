8b22a39b33c0aec2ec8625300c9a8088911d16bf
unknown <ryanchehab20@gmail.com>
2024-10-27 21:41:20 +0200

    finished the tree

--- ex3.js
+++ ex3.js
@@ -4,19 +4,27 @@ height = parseInt(height);
 // main condition
 if (!isNaN(height) && height!== "" && height>0){
 
-    console.log(height)
+    // building the container div
     let container = document.createElement("div")
     container.classList.add('container')
     
     // generating the tree
     for (let x = 0; x < height; x++) {
-        let volume = " ".repeat(height - x - 1)+ "*" + "  |  ".repeat(x) + "*";
-        let tree = document.createElement("div");
-        tree.innerHTML = volume;
-        container.appendChild(tree);
+        let stars = `<span class="star">*</span>`
+        let volume = `<span class="volume">|</span>`.repeat(x)
+        let padd = " ".repeat(height - x)
+
+        let tree = document.createElement('div');
+        tree.classList.add('tree');
+        tree.innerHTML = padd + stars + volume + stars;
+
+        // adding the tree div to the document
+        container.appendChild(tree)
     }
-    
+
+    // adding the whole picture to the document
     document.body.appendChild(container)
+    
 }
 else{
     alert("Please enter a valid height")
